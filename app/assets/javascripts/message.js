b676d4199ad06fa28190b66cb9e950a17ebf8770
Sora-bs <aozora@icloud.com>
2019-04-22 10:43:15 +0900

    Commit-asynchronous-communication

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,32 @@
 $(function(){
+    function buildHtml(message){
+        var html = `.text
+                        - if ${message.content}.present?
+                            %p.text
+                            = ${message.content}
+                        = image_tag ${message.image}.url, class: 'text' if ${message.image}.present?`
+        return html;
+    }
+
     $('#new_message').on('submit', function(e){
         e.preventDefault();
-        console.log(this)
         var formData = new FormData(this);
+        var url = $(this).attr('action');
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: formData,
+            dataType: 'json',
+            processData: false,
+            contentType: false
+        })
+        .done(function(message){
+            var html = buildHtml(message);
+            $('.text').append(html)
+            $('.text').animate({scrollTop: $('.text')[0].scrollHeight}, 'fast');
+        })
+        .fail(function(){
+            alert('error');
+          })
     })
 })
--- app/views/messages/create.json.jbuilder
+++ app/views/messages/create.json.jbuilder
@@ -1,4 +1,4 @@
 json.content @message.content
-json.image @message.image
+json.image @message.image_url
 json.group_id @message.group.id
 json.user_id @message.user.id
